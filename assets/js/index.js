999641b16586669e4f9a1ba715cf1325d0e89b74
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-01 01:35:27 -0500

    track time changes and change textentry color for past, present, future

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,30 @@
 var todaysDate = moment().format('dddd, MMMM Do');
 // add todaysDate to html header 
 $("#currentDay").html(todaysDate);
+
+var rows = document.getElementsByClassName("row");
+var currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+    var rowIdString = row.id, 
+    rowHour;
+    if (rowIdString) {
+        rowHour = parseInt(rowIdString);
+    }
+    if (rowHour) {
+        // compares row id to current hour and sets color
+        if (currentHour === rowHour) {
+           setColor(row, "red");
+        } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+            setColor(row, "green");
+        } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+            setColor(row, "lightgrey");
+        } else {
+            setColor(row, "white");
+        }
+    }
+});
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+}
