ce5cf6b7233f1d6f8696082b28f836e5341aef93
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-04 00:34:40 -0500

    load saved tasks on page refresh from localstorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,20 +6,27 @@ var todaysDate = moment().format('dddd, MMMM Do');
 // add todaysDate to html header 
 $("#currentDay").html(todaysDate);
 
+// gets .row elements from index
 var rows = document.getElementsByClassName("row");
 var currentHour = parseInt(moment().format('H'));
+// creates an array from each .row element 
 Array.from(rows).forEach(row => {
+    // sets html ids to rowHour array
     var rowIdString = row.id, 
     rowHour;
     if (rowIdString) {
+        // converts hour/id string to numbers 
         rowHour = parseInt(rowIdString);
     }
     if (rowHour) {
-        // compares row id to current hour and sets color
+        // compares row id to current hour and sets color accordingly
         if (currentHour === rowHour) {
+            // green
            setColor(row, "#ff6961");
         } else if ((currentHour < rowHour)) {
+            // red
             setColor(row, "#77dd77");
+            // grey
         } else {
             setColor(row, "#d3d3d3");
         }
@@ -32,8 +39,21 @@ $(".saveBtn").on("click", function () {
      var hour = $(this).parent().attr("id");
     // save tasks to localStorage
     localStorage.setItem(hour, tasks);
-;})
+})
 
+// load local storage saved tasks for each row/hour 
+$("#8 .text-entry").val(localStorage.getItem(8));
+$("#9 .text-entry").val(localStorage.getItem(9));
+$("#10 .text-entry").val(localStorage.getItem(10));
+$("#11 .text-entry").val(localStorage.getItem(11));
+$("#12 .text-entry").val(localStorage.getItem(12));
+$("#13 .text-entry").val(localStorage.getItem(13));
+$("#14 .text-entry").val(localStorage.getItem(14));
+$("#15 .text-entry").val(localStorage.getItem(15));
+$("#16 .text-entry").val(localStorage.getItem(16));
+$("#17 .text-entry").val(localStorage.getItem(17));
+
+// function change row element background colors 
 function setColor(element, color) {
     element.style.backgroundColor = color;
 }
